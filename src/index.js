503c2b43cbf256412cb851a7d882da827af20222
Katriaryna Mazgo <ekaterina.mozgo@mail.ru>
2018-09-25 23:42:00 +0300

    longest-consecutive-sequence/fix: implemented function longestConsecutiveLength

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+
+    if (array.length === 0) {
+        return 0;
+    }
+
+    let obj = {};
+
+    for (let i = 0, len = array.length; i < len; i++) {
+        obj[array[i]] = array[i];
+    }
+
+    let counter = 1;
+    let maxCounter = 1;
+
+    for (let key in obj) {
+        if (+(obj[+key]) + 1 === +(obj[+key + 1])) {
+            counter++;
+        } else {
+            if (counter > maxCounter) {
+                maxCounter = counter;
+            }
+            counter = 1;
+        }
+    }
+    return maxCounter;
+
 }
